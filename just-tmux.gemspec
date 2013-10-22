a8d6abaf11913cdcbcb0217a5abffa209dfeda70
James <james@ralina.(none)>
2013-10-22 12:18:23 +0100

    trivial refactoring

--- just-tmux.gemspec
+++ just-tmux.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'just-tmux'
-  s.version = '0.1.5'
+  s.version = '0.1.6'
   s.summary = 'just-tmux'
   s.authors = ['James Robertson']
   s.files = Dir['lib/**/*.rb']
--- lib/just-tmux.rb
+++ lib/just-tmux.rb
@@ -8,7 +8,7 @@ class JustTmux
 
   attr_reader :to_s
 
-
+  
   # example raw dynarex file:
   #
   # config=<<EOF
@@ -34,7 +34,6 @@ class JustTmux
       x[:dir] = default_dir if x[:dir].empty?
       x
     end
-
     
     @to_s = new_session(session_name, h[0][:name], shell, 
       "cd #{h[0][:dir]} && " + h[0][:command]) +  h[1..-1].map \
@@ -45,15 +44,15 @@ class JustTmux
 
   private
 
-  def new_window(window_name, shell, command)
-    "tmux new-window -n %s %s\ntmux send-keys '%s' Enter\n" % 
-        [window_name, shell, command]
-  end
-
   def new_session(session_name, window_name, shell, command)
-    r = "tmux new-session -d -s %s -n %s '%s'\n" % 
-      [session_name, window_name, shell]
-    r + "tmux send-keys '%s' Enter\n" % [command]
+    "tmux new-session -d -s %s -n %s '%s'\n%s" % 
+      [session_name, window_name, shell, send_keys(command)]
   end
+  
+  def new_window(window_name, shell, command)
+    "tmux new-window -n %s %s\n%s" % [window_name, shell, send_keys(command)]
+  end  
+
+  def send_keys(command)  "tmux send-keys '%s' Enter\n" % command  end
 
 end
